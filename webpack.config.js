4df795c8ae0c5adb9fd1f759c8d16f4a9122ddd9
Priscila Méndez <priscymendez_26@hotmail.com>
2019-06-15 22:36:19 -0600

    Fixing build options

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,55 @@
 var webpack = require("webpack");
 var path = require("path");
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 var sourcePath = path.resolve(__dirname, "src");
 var jsSourcePath = path.resolve(__dirname, "src");
 var buildPath = path.resolve(__dirname, "dist");
 
 var config = {
-    mode: 'development',
-    entry: sourcePath + "/main.js",
-    output: {
-        path: buildPath,
-        publicPath: "/dist/",
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                exclude: /node_modules/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                loader: "style-loader!css-loader"
-            }
-        ]
-    },
-    plugins: [
-      new VueLoaderPlugin(),
-      new HtmlWebpackPlugin({
-        template: './public/index.html'
-      }),
-      new Dotenv()
-    ],
-    resolve: {
-        extensions: [".js", ".jsx"],
-        modules: [            
-            "node_modules",
-            jsSourcePath
-        ],
-        alias: {
-            'vuejs-datatable': 'vuejs-datatable/dist/vuejs-datatable.esm.js',
+  mode: "development",
+  entry: sourcePath + "/main.js",
+  output: {
+    path: buildPath,
+    // publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-    },
-    devServer: {
-        port: 3003,
-        publicPath: "http://localhost:3003/dist/",
-        contentBase: path.join(__dirname, "public/"),
-
-    },
-    devtool: "eval-source-map"
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ]
+  },
+  plugins: [new VueLoaderPlugin(), new Dotenv()],
+  resolve: {
+    extensions: [".js", ".jsx"],
+    modules: ["node_modules", jsSourcePath],
+    alias: {
+      "vuejs-datatable": "vuejs-datatable/dist/vuejs-datatable.esm.js"
+    }
+  },
+  devServer: {
+    port: 3003,
+    publicPath: "http://localhost:3003/dist/",
+    contentBase: path.join(__dirname, "public/")
+  },
+  devtool: "eval-source-map"
 };
 
 module.exports = config;
